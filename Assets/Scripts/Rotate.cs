2fd835f26654f8a5f8c2aa48ecbba3509cde7b1b
bickhofe <peter@bickhofe.de>
2018-07-03 08:08:09 +0200

    init

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public Vector3 axis;
+    public float speed;
+    public bool active = false;
+
+	public void toggle () {
+        if (!active) active = true;
+        else active = false;
+	}
+	
+	void Update () {
+        if (active) transform.Rotate(axis * speed * Time.deltaTime);
+	}
+}
